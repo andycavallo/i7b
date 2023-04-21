6574d44ec3822a85a191256b6b33fa15b5a0969a
andycavallo <131465688+andycavallo@users.noreply.github.com>
2023-04-21 17:19:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,22 +11,29 @@ fetch(apiUrl)
     const rows = data.values;
     let content = '<table>';
 
+    // Add table headers
+    content += `<tr><th>Rank</th><th>Clan Name</th><th>Column H</th><th>Column I</th><th>Column L</th><th>Spostamento</th></tr>`;
+
     rows.forEach((row) => {
-      content += '<tr>';
-      const filteredRow = [
-        row[0], // Column A
-        row[2], // Column C
-        row[7], // Column H
-        row[8], // Column I
-        row[11], // Column L
-      ];
-      filteredRow.forEach((cell) => {
-        content += `<td>${cell}</td>`;
-      });
-      content += '</tr>';
+      const rank = parseInt(row[0]);
+      const currentClan = row[2];
+
+      let targetClan;
+      if (rank >= 1 && rank <= 50) {
+        targetClan = 'I7B';
+      } else if (rank >= 51 && rank <= 101) {
+        targetClan = 'I7B2';
+      } else {
+        targetClan = 'I7B3';
+      }
+
+      const spostamento = currentClan !== targetClan ? `${currentClan} -> ${targetClan}` : '';
+
+      content += `<tr><td>${row[0]}</td><td>${row[2]}</td><td>${row[7]}</td><td>${row[8]}</td><td>${row[11]}</td><td>${spostamento}</td></tr>`;
     });
 
     content += '</table>';
     document.getElementById('content').innerHTML = content;
   })
   .catch((error) => console.error('Error fetching data:', error));
+
