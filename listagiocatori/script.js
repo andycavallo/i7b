2abbc182547092f965a427cae3f1e7d9e9854c7d
andycavallo <131465688+andycavallo@users.noreply.github.com>
2023-04-28 14:14:46 +0200

    Update script.js

--- listagiocatori/script.js
+++ listagiocatori/script.js
@@ -5,49 +5,37 @@ const sheetName = 'LastDay'; // Replace with your sheet name if different
 const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}!A1:V?key=${apiKey}`;
 
 fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        const rows = data.values;
-        rows.sort((a, b) => {
-            const clanA = a[2] || '';
-            const clanB = b[2] || '';
-            const trophiesA = parseInt(a[8]) || 0;
-            const trophiesB = parseInt(b[8]) || 0;
+  .then((response) => response.json())
+  .then((data) => {
+    const rows = data.values;
+    rows.shift(); // Remove the header row
 
-            if (clanA === clanB) {
-                return trophiesB - trophiesA;
-            } else {
-                return clanA.localeCompare(clanB);
-            }
-        });
-
-        let content = '';
-
-        rows.forEach((row, rowIndex) => {
-            const clanName = row[2] || '';
-            const id = row[6] || '';
-            const name = row[7] || '';
-            const trophies = row[8] || '';
-            const grado = row[19] || '';
-            const nomeTelegram = row[20] || '';
-            let usernameTelegram = row[21] || '';
-            if (usernameTelegram) {
-                usernameTelegram = `<a href='https://t.me/${usernameTelegram}'>${usernameTelegram}</a>`;
-            }
-
-            let rowClass = '';
-            if (grado.includes('Generale')) {
-                rowClass = 'generale';
-            } else if (grado.includes('Capitano')) {
-                rowClass = 'capitano';
-            } else if (grado.includes('Tenente')) {
-                rowClass = 'tenente';
-            }
-
-        content += `<tr class="${rowClass}"><td>${clanName}</td><td>${id}</td><td>${name}</td><td>${trophies}</td><td>${grado}</td><td>${nomeTelegram}</td><td>${usernameTelegram}</td></tr>`;
+    rows.sort((a, b) => {
+      if (a[2] === b[2]) {
+        return b[8] - a[8]; // Sort by Trophies if Clan name is the same
+      }
+      return a[2].localeCompare(b[2]); // Sort by Clan name
     });
 
-    document.getElementById('content').innerHTML = content;
-})
-.catch((error) => console.error('Error fetching data:', error));
+    rows.forEach((row) => {
+      const clanName = row[2] || '';
+      const id = row[6] || '';
+      const name = row[7] || '';
+      const trophies = row[8] || '';
+      const grado = row[19] || '';
+      const nomeTelegram = row[20] || '';
+      const usernameTelegram = row[21] ? `<a href="https://t.me/${row[21]}" target="_blank">${row[21]}</a>` : '';
 
+      let rowClass = '';
+      if (grado.includes('Generale')) {
+        rowClass = 'generale';
+      } else if (grado.includes('Capitano')) {
+        rowClass = 'capitano';
+      } else if (grado.includes('Tenente')) {
+        rowClass = 'tenente';
+      }
+      const content = `<tr class="${rowClass}"><td>${clanName}</td><td>${id}</td><td>${name}</td><td>${trophies}</td><td>${grado}</td><td>${nomeTelegram}</td><td>${usernameTelegram}</td></tr>`;
+      document.getElementById('content').insertAdjacentHTML('beforeend', content);
+    });
+  })
+  .catch((error) => console.error('Error fetching data:', error));
