bd1757ae54dbde8e84d80d89a06c399ee1dc6aaa
andycavallo <131465688+andycavallo@users.noreply.github.com>
2023-05-17 15:47:13 +0200

    Update script.js

--- listagiocatori/script.js
+++ listagiocatori/script.js
@@ -50,7 +50,39 @@ function updateTable(clanFilter = '', showMultipleAccounts = false) {
       filteredRows = filteredRows.filter(row => usernameToRows[row[21]].length > 1);
   }
 
-  // ... Rest of your function
+  filteredRows.sort((a, b) => {
+    if (a[2] === b[2]) {
+        return b[8] - a[8]; // Sort by Trophies if Clan name is the same
+    }
+    return a[2].localeCompare(b[2]); // Sort by Clan name
+});
+
+const tbody = document.getElementById('content').querySelector('tbody');
+tbody.innerHTML = ''; // Clear the table body
+
+filteredRows.forEach(row => {
+    const clanName = row[2];
+    const id = row[6] || '';
+    const name = row[7] || '';
+    const trophies = row[8] || '';
+    const grado = row[19] || '';
+    const nomeTelegram = row[20] || '';
+    const usernameTelegram = row[21] ? `<a href="https://t.me/${row[21]}" target="_blank">${row[21]}</a>` : '';
+    const nomeDiscord = row[22] || '';
+
+    let rowClass = '';
+    if (grado.includes('Generale')) {
+        rowClass = 'generale';
+    } else if (grado.includes('Capitano')) {
+        rowClass = 'capitano';
+    } else if (grado.includes('Tenente')) {
+        rowClass = 'tenente';
+    }
+
+    const content = `<tr class="${rowClass}"><td>${clanName}</td><td>${id}</td><td>${name}</td><td>${trophies}</td><td>${grado}</td><td>${nomeTelegram}</td><td>${usernameTelegram}</td><td>${nomeDiscord}</td></tr>`;
+    tbody.insertAdjacentHTML('beforeend', content);
+});
+
 }
 
 function createSummaryTable() {
