dcdb2a11328798e4bb33ee42325c3d98546be438
andycavallo <131465688+andycavallo@users.noreply.github.com>
2023-12-11 11:43:06 +0100

    Update script.js

--- listagiocatori/script.js
+++ listagiocatori/script.js
@@ -44,63 +44,15 @@ fetch(apiUrl)
     })
     .catch((error) => console.error('Error fetching data:', error));
 
-function updateTable(clanFilter = '', showMultipleAccounts = false) {
-    let filteredRows = clanFilter ? allRows.filter(row => row[2] === clanFilter) : [...allRows];
-
-    if (showMultipleAccounts) {
-        filteredRows = filteredRows.filter(row => {
-            const username = row[21];
-            return username && usernameToRows[username].length > 1;
-        });
-
-        filteredRows.sort((a, b) => {
-            const aUsername = a[21];
-            const bUsername = b[21];
-
-            if (aUsername !== bUsername) {
-                return aUsername.localeCompare(bUsername);
-            }
-
-            if (a[2] !== b[2]) {
-                return a[2].localeCompare(b[2]);
-            }
-
-            return b[8] - a[8];
-        });
-    } else {
-        filteredRows.sort((a, b) => {
-            if (a[2] === b[2]) {
-                return b[8] - a[8];
-            }
-            return a[2].localeCompare(b[2]);
-        });
-    }
-
-    const tbody = document.getElementById('content').querySelector('tbody');
-    tbody.innerHTML = '';
-
-    filteredRows.forEach(row => {
-        const clanName = row[2];
-        const id = row[6] || '';
-        const name = row[7] || '';
-        const trophies = row[8] || '';
-        const grado = row[19] || '';
-        const nomeTelegram = row[20] || '';
-        const usernameTelegram = row[21] ? `<a href="https://t.me/${row[21]}" target="_blank">${row[21]}</a>` : '';
-        const nomeDiscord = row[22] || '';
-
-        let rowClass = '';
-        if (grado.includes('Generale')) {
-            rowClass = 'generale';
-        } else if (grado.includes('Capitano')) {
-            rowClass = 'capitano';
-        } else if (grado.includes('Tenente')) {
-            rowClass = 'tenente';
-        }
-
-        const content = `<tr class="${rowClass}"><td>${clanName}</td><td>${id}</td><td>${name}</td><td>${trophies}</td><td>${grado}</td><td>${nomeTelegram}</td><td>${usernameTelegram}</td><td>${nomeDiscord}</td></tr>`;
-        tbody.insertAdjacentHTML('beforeend', content);
-    });
+function calculate_score(position, trophies) {
+    if (position === 0) multiplier = 0.5;
+    else if (position < 10) multiplier = 0.4;
+    else if (position < 20) multiplier = 0.3;
+    else if (position < 30) multiplier = 0.2;
+    else if (position < 40) multiplier = 0.1;
+    else if (position < 50) multiplier = 0.05;
+    else multiplier = 0;
+    return parseFloat(trophies) * multiplier;
 }
 
 function simulateScoreAfterMovements() {
@@ -130,33 +82,25 @@ function createSummaryTable() {
     const summaryTbody = document.getElementById('summary-content').querySelector('tbody');
     summaryTbody.innerHTML = '';
 
+    const scoresAfterMovements = simulateScoreAfterMovements();
     const clans = ['I7B', 'I7B2', 'I7B3'];
 
     clans.forEach(clan => {
         const playersInClan = allRows.filter(row => row[2] === clan);
-        const sortedPlayersInClan = playersInClan.sort((a, b) => b[8] - a[8]).slice(0, 50); // Ordina i giocatori per coppe
+        const sortedPlayersInClan = playersInClan.sort((a, b) => b[8] - a[8]).slice(0, 50);
 
         let clanScore = 0;
         sortedPlayersInClan.forEach((row, index) => {
-            let multiplier = 0;
-            if (index === 0) multiplier = 0.5;
-            else if (index < 10) multiplier = 0.4;
-            else if (index < 20) multiplier = 0.3;
-            else if (index < 30) multiplier = 0.2;
-            else if (index < 40) multiplier = 0.1;
-            else if (index < 50) multiplier = 0.05;
-            clanScore += parseFloat(row[8]) * multiplier; // Usa parseFloat per gestire numeri decimali
+            clanScore += calculate_score(index, row[8]);
         });
 
-        clanScore = Math.round(clanScore); // Arrotonda il punteggio totale alla fine
+        clanScore = Math.round(clanScore);
+        const scoreAfterMovements = scoresAfterMovements[clan];
 
         const telegramInClan = sortedPlayersInClan.filter(row => row[20]).length;
         const discordInClan = sortedPlayersInClan.filter(row => row[22]).length;
 
-        const summaryContent = `<tr><td>${clan}</td><td>${sortedPlayersInClan.length}/50</td><td>${telegramInClan}/50</td><td>${discordInClan}/50</td><td>${clanScore}</td></tr>`;
+        const summaryContent = `<tr><td>${clan}</td><td>${sortedPlayersInClan.length}/50</td><td>${telegramInClan}/50</td><td>${discordInClan}/50</td><td>${clanScore}</td><td>${scoreAfterMovements}</td></tr>`;
         summaryTbody.insertAdjacentHTML('beforeend', summaryContent);
-
-        const scoresAfterMovements = simulateScoreAfterMovements();
     });
 }
-
