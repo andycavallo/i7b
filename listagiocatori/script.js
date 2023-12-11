fd46d0b6ffaab41bf6ff4a0121718877a95c14dc
andycavallo <131465688+andycavallo@users.noreply.github.com>
2023-12-11 10:14:27 +0100

    Update script.js

--- listagiocatori/script.js
+++ listagiocatori/script.js
@@ -1,4 +1,4 @@
-const apiKey = 'AIzaSyAMwp2PrmXiQj2Qyi0v3TJVWFD5Jl0eF2I'; 
+const apiKey = 'AIzaSyAMwp2PrmXiQj2Qyi0v3TJVWFD5Jl0eF2I';
 const sheetId = '16gHjqHQJCbZApcKYUCtJkcoIsIKcJ30VkK-OVaYqwUU';
 const sheetName = 'LastDay';
 
@@ -8,127 +8,124 @@ let allRows = [];
 let usernameToRows = {};
 
 fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    allRows = data.values.filter(row => row[2]); // Exclude rows with an empty Clan Name
-    allRows.shift(); // Remove the header row
+    .then((response) => response.json())
+    .then((data) => {
+        allRows = data.values.filter(row => row[2]);
+        allRows.shift();
 
-    // Create a mapping of usernames to rows
-    usernameToRows = allRows.reduce((map, row) => {
-        const username = row[21];
-        if (username) { // Only map rows with a username
-            if (!map[username]) {
-                map[username] = [];
+        usernameToRows = allRows.reduce((map, row) => {
+            const username = row[21];
+            if (username) {
+                if (!map[username]) {
+                    map[username] = [];
+                }
+                map[username].push(row);
             }
-            map[username].push(row);
-        }
-        return map;
-    }, {});
+            return map;
+        }, {});
 
-    const uniqueClans = [...new Set(allRows.map(row => row[2]))].filter(Boolean).sort(); // Exclude empty Clan Names and sort
+        const uniqueClans = [...new Set(allRows.map(row => row[2]))].filter(Boolean).sort();
 
-    const dropdown = document.getElementById('clan-filter');
-    dropdown.innerHTML = '<option value="">All Clans</option>' + uniqueClans.map(clan => `<option value="${clan}">${clan}</option>`).join('');
+        const dropdown = document.getElementById('clan-filter');
+        dropdown.innerHTML = '<option value="">All Clans</option>' + uniqueClans.map(clan => `<option value="${clan}">${clan}</option>`).join('');
 
-    dropdown.addEventListener('change', (event) => {
-      const selectedClan = event.target.value;
-      updateTable(selectedClan, document.getElementById('multi-account-checkbox').checked);
-    });
+        dropdown.addEventListener('change', (event) => {
+            const selectedClan = event.target.value;
+            updateTable(selectedClan, document.getElementById('multi-account-checkbox').checked);
+        });
 
-    const checkbox = document.getElementById('multi-account-checkbox');
-    checkbox.addEventListener('change', (event) => {
-        updateTable(document.getElementById('clan-filter').value, event.target.checked);
-    });
+        const checkbox = document.getElementById('multi-account-checkbox');
+        checkbox.addEventListener('change', (event) => {
+            updateTable(document.getElementById('clan-filter').value, event.target.checked);
+        });
 
-    updateTable();
-    createSummaryTable();
-  })
-  .catch((error) => console.error('Error fetching data:', error));
+        updateTable();
+        createSummaryTable();
+    })
+    .catch((error) => console.error('Error fetching data:', error));
 
 function updateTable(clanFilter = '', showMultipleAccounts = false) {
-  let filteredRows = clanFilter ? allRows.filter(row => row[2] === clanFilter) : [...allRows];
+    let filteredRows = clanFilter ? allRows.filter(row => row[2] === clanFilter) : [...allRows];
 
-  if (showMultipleAccounts) {
-    filteredRows = filteredRows.filter(row => {
-        const username = row[21];
-        return username && usernameToRows[username].length > 1;
-    });
+    if (showMultipleAccounts) {
+        filteredRows = filteredRows.filter(row => {
+            const username = row[21];
+            return username && usernameToRows[username].length > 1;
+        });
 
-    // Sorting rows - username first, then by Clan name and Trophies
-    filteredRows.sort((a, b) => {
-        const aUsername = a[21];
-        const bUsername = b[21];
+        filteredRows.sort((a, b) => {
+            const aUsername = a[21];
+            const bUsername = b[21];
 
-        if (aUsername !== bUsername) {
-            return aUsername.localeCompare(bUsername); // Sort by username
-        } 
+            if (aUsername !== bUsername) {
+                return aUsername.localeCompare(bUsername);
+            }
 
-        if (a[2] !== b[2]) {
-            return a[2].localeCompare(b[2]); // If usernames are the same, sort by Clan name
-        }
+            if (a[2] !== b[2]) {
+                return a[2].localeCompare(b[2]);
+            }
 
-        return b[8] - a[8]; // If Clan names are also the same, sort by Trophies
-    });
-  } else {
-    filteredRows.sort((a, b) => {
-        if (a[2] === b[2]) {
-            return b[8] - a[8]; // Sort by Trophies if Clan name is the same
-        }
-        return a[2].localeCompare(b[2]); // Sort by Clan name
-    });
-  }
-
-  const tbody = document.getElementById('content').querySelector('tbody');
-  tbody.innerHTML = ''; // Clear the table body
-
-  filteredRows.forEach(row => {
-    const clanName = row[2];
-    const id = row[6] || '';
-    const name = row[7] || '';
-    const trophies = row[8] || '';
-    const grado = row[19] || '';
-    const nomeTelegram = row[20] || '';
-    const usernameTelegram = row[21] ? `<a href="https://t.me/${row[21]}" target="_blank">${row[21]}</a>` : '';
-    const nomeDiscord = row[22] || '';
-
-    let rowClass = '';
-    if (grado.includes('Generale')) {
-      rowClass = 'generale';
-    } else if (grado.includes('Capitano')) {
-      rowClass = 'capitano';
-    } else if (grado.includes('Tenente')) {
-      rowClass = 'tenente';
+            return b[8] - a[8];
+        });
+    } else {
+        filteredRows.sort((a, b) => {
+            if (a[2] === b[2]) {
+                return b[8] - a[8];
+            }
+            return a[2].localeCompare(b[2]);
+        });
     }
 
-    const content = `<tr class="${rowClass}"><td>${clanName}</td><td>${id}</td><td>${name}</td><td>${trophies}</td><td>${grado}</td><td>${nomeTelegram}</td><td>${usernameTelegram}</td><td>${nomeDiscord}</td></tr>`;
-    tbody.insertAdjacentHTML('beforeend', content);
-  });
+    const tbody = document.getElementById('content').querySelector('tbody');
+    tbody.innerHTML = '';
+
+    filteredRows.forEach(row => {
+        const clanName = row[2];
+        const id = row[6] || '';
+        const name = row[7] || '';
+        const trophies = row[8] || '';
+        const grado = row[19] || '';
+        const nomeTelegram = row[20] || '';
+        const usernameTelegram = row[21] ? `<a href="https://t.me/${row[21]}" target="_blank">${row[21]}</a>` : '';
+        const nomeDiscord = row[22] || '';
+
+        let rowClass = '';
+        if (grado.includes('Generale')) {
+            rowClass = 'generale';
+        } else if (grado.includes('Capitano')) {
+            rowClass = 'capitano';
+        } else if (grado.includes('Tenente')) {
+            rowClass = 'tenente';
+        }
+
+        const content = `<tr class="${rowClass}"><td>${clanName}</td><td>${id}</td><td>${name}</td><td>${trophies}</td><td>${grado}</td><td>${nomeTelegram}</td><td>${usernameTelegram}</td><td>${nomeDiscord}</td></tr>`;
+        tbody.insertAdjacentHTML('beforeend', content);
+    });
 }
 
 function createSummaryTable() {
-  const summaryTbody = document.getElementById('summary-content').querySelector('tbody');
-  summaryTbody.innerHTML = ''; // Clear the summary table body
+    const summaryTbody = document.getElementById('summary-content').querySelector('tbody');
+    summaryTbody.innerHTML = '';
 
-  const clans = ['I7B', 'I7B2', 'I7B3'];
+    const clans = ['I7B', 'I7B2', 'I7B3'];
 
-  let totalPlayers = 0;
-  let totalTelegram = 0;
-  let totalDiscord = 0;
+    clans.forEach(clan => {
+        const playersInClan = allRows.filter(row => row[2] === clan).slice(0, 50);
+        const telegramInClan = playersInClan.filter(row => row[20]).length;
+        const discordInClan = playersInClan.filter(row => row[22]).length;
 
-  clans.forEach(clan => {
-    const playersInClan = allRows.filter(row => row[2] === clan);
-    const telegramInClan = playersInClan.filter(row => row[20]).length;
-    const discordInClan = playersInClan.filter(row => row[22]).length;
+        let clanScore = 0;
+        playersInClan.forEach((row, index) => {
+            let multiplier = 0;
+            if (index < 10) multiplier = 0.4;
+            else if (index >= 10 && index < 20) multiplier = 0.3;
+            else if (index >= 20 && index < 30) multiplier = 0.2;
+            else if (index >= 30 && index < 40) multiplier = 0.1;
+            else if (index >= 40) multiplier = 0.05;
+            clanScore += parseInt(row[8]) * multiplier;
+        });
 
-    totalPlayers += playersInClan.length;
-    totalTelegram += telegramInClan;
-    totalDiscord += discordInClan;
-
-    const summaryContent = `<tr><td>${clan}</td><td>${playersInClan.length}/50</td><td>${telegramInClan}/50</td><td>${discordInClan}/50</td></tr>`;
-    summaryTbody.insertAdjacentHTML('beforeend', summaryContent);
-  });
-  
-  const totalContent = `<tr class="total-row"><td>Total</td><td>${totalPlayers}/150</td><td>${totalTelegram}/150</td><td>${totalDiscord}/150</td></tr>`;
-  summaryTbody.insertAdjacentHTML('beforeend', totalContent);
+        const summaryContent = `<tr><td>${clan}</td><td>${playersInClan.length}/50</td><td>${telegramInClan}/50</td><td>${discordInClan}/50</td><td>${clanScore.toFixed(2)}</td></tr>`;
+        summaryTbody.insertAdjacentHTML('beforeend', summaryContent);
+    });
 }
-
